ee4a4b549fb76a92315e22b2e2e12ace487e4998
safejulian <julian@safeadvisory.co.nz>
2025-09-18 11:17:15 +1200

    More fixes

--- manifest.js
+++ manifest.js
@@ -5,18 +5,20 @@ const path = require('path');
 // read the retirejs json file
 const retirejsFilePath = path.join(__dirname, 'retire.json');
 const retirejsData = JSON.parse(fs.readFileSync(retirejsFilePath, 'utf8'))['data'];
-const manifests = [];
+const resolved = {};
 const sha = process.env.GITHUB_SHA || 'not-an-sha';
 const ref = process.env.GITHUB_REF || 'refs/heads/foo';
-retirejsData.forEach(function(item) {
+retirejsData.forEach(function (item) {
     let component = item.results[0].component;
     let version = item.results[0].version;
-    manifests.push({
-        ["pkg:npm/" + component + "@" + version]: {
-            'package_url': "pkg:npm/" + component + "@" + version,
-            'relationship': 'direct',
-            'dependencies': [] }})
+    let key = "pkg:npm/" + component + "@" + version;
+    resolved[key] = {
+        'package_url': key,
+        'relationship': 'direct',
+        'dependencies': []
+    }
 });
+
 const submission = {
     sha: sha,
     ref: ref,
@@ -31,6 +33,6 @@ const submission = {
         version: '5.3.0',
         url: 'https://retirejs.github.io/retire.js/'
     },
-    manifests: {"retirejs": {name: "retirejs", "resolved": manifests}}
+    manifests: {"retirejs": {name: "retirejs", "resolved": resolved}}
 };
 console.log(JSON.stringify(submission));
